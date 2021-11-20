98a5a9940b4571b61f60cbd7daae81e3452a19f1
Wilson Lopes de Amorim <40276163+WilsonAmorim@users.noreply.github.com>
2021-11-20 09:50:33 -0300

    Base criada

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # nodests-canil
+Projeto fei no modulo do curso Node + Typescript
+
+### pré-requisitos globais:
+`npm install -g nodemon typescript ts-node`
+
+### Intalação
+`npm install`
+
+### Para rodar o projeto
+`npm run start-dev`
---
+++ src/server.ts
@@ -0,0 +1,21 @@
+import express, { Request, Response} from 'express';
+import path from 'path';
+import mustache from 'mustache-express';
+import dotenv from 'dotenv';
+import  mainRoutes from './routes/index';
+
+dotenv.config();
+
+const server = express();
+server.set('view engine', 'mustache');
+server.set('views', path.join(__dirname, 'views'));
+server.engine('mustache', mustache());
+server.use(express.static( path.join(__dirname, '../public')));
+server.use(express.urlencoded({extended: true}));
+server.use(mainRoutes);
+// Rotas
+
+server.use((req, res)=>{
+  res.send('Página não encontrada');
+})
+server.listen(process.env.PORT);
